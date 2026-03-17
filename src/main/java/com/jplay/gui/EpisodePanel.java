90ccdf241bbf72548b029dd93b93dc3ca8075307
Alfred <alfred@stensatter.se>
2026-03-17 01:06:57 +0100

    began on seasons list

--- src/main/java/com/jplay/gui/EpisodePanel.java
+++ src/main/java/com/jplay/gui/EpisodePanel.java
@@ -1,18 +1,27 @@
 package com.jplay.gui;
 
 import java.awt.BorderLayout;
+import java.awt.CardLayout;
+import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.ListSelectionModel;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
 import com.jplay.Main;
 import com.jplay.Playable;
@@ -22,6 +31,12 @@ import com.jplay.loaders.SQLitePlayableLoader;
 public class EpisodePanel extends JPanel {
 
 		JScrollPane scrollPane = new JScrollPane();
+		JPanel episodePanel = new JPanel(new CardLayout());
+
+		JPanel seasonsPanel = new JPanel();
+		JList<String> seasons = new JList<>();
+		DefaultListModel<String> model = new DefaultListModel<>();
+		ArrayList<Playable> playables = new ArrayList<>();
 		
 		public EpisodePanel() {
 				setLayout(new BorderLayout());
@@ -30,26 +45,61 @@ public class EpisodePanel extends JPanel {
 				etitle.setFont(new Font("Serif", Font.PLAIN, 24));
 				add(etitle, BorderLayout.NORTH);
 
+				seasons = new JList<String>(model);
+				seasons.addListSelectionListener(new ListSelectionListener() {
+								@Override
+								public void valueChanged(ListSelectionEvent e) {
+										setSeason(Integer.parseInt(seasons.getSelectedValue()));
+								}
+						});
+				
+        seasons.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+				seasonsPanel.add(seasons);
+
+				setSeason(1);
+
+
+				add(seasons,BorderLayout.WEST);
 				add(scrollPane, BorderLayout.CENTER);
+
 		}
 
-		public void setPlayable(Playable playable) {
+		public void setSeason(int season) {
 				JPanel episodeListPanel = new JPanel();
         episodeListPanel.setLayout(new BoxLayout(episodeListPanel, BoxLayout.Y_AXIS));
 
 				SQLitePlayableLoader loader = new SQLitePlayableLoader();
-
 				
-        for (Playable episode : loader.getAllEpisodes(playable.title)) {
 
-						EpisodeRow row = new EpisodeRow(episode);
+        for (Playable episode : playables) {
+						if (episode.season != season) continue;
 						
+						EpisodeRow row = new EpisodeRow(episode);
             episodeListPanel.add(row);
             episodeListPanel.add(Box.createVerticalStrut(5)); // space between episodes
+
+
+						if(!model.contains(String.valueOf(episode.season))) {
+								model.addElement(String.valueOf(episode.season));
+						}
         }
+				
 				remove(scrollPane);
 				scrollPane = new JScrollPane(episodeListPanel);
-				add(scrollPane);				
+				add(scrollPane);
+				
+				validate();
+				repaint();
 		}
-		
+
+		public void setPlayable(Playable playable) {
+				SQLitePlayableLoader loader = new SQLitePlayableLoader();
+
+				for(Playable episode : loader.getAllEpisodes(playable.title)) {
+						playables.add(episode);
+						if(!model.contains(String.valueOf(episode.season))) {
+								model.addElement(String.valueOf(episode.season));
+						}
+				}
+		}
 }
